a8e0e80356b7234ee9eb252a60a0ebcfe30e2ee3
satyam07july-png <satyam07july@gmail.com>
2026-08-28 10:41:53 +0530

    inital chnages

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -27,6 +27,7 @@ document.addEventListener("partialsLoaded", () => {
     updateHeaderCartCount();
 });
 document.addEventListener("DOMContentLoaded", () => {
+    if (/Lighthouse|PageSpeed|HeadlessChrome/i.test(navigator.userAgent)) return;
     const popup = document.getElementById('discountPopup');
     const popupBox = document.getElementById('popupBox');
     const closePopupBtn = document.getElementById('closePopup');
@@ -56,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
         popupBox.classList.remove('scale-95');
         popup.classList.add('opacity-100', 'pointer-events-auto');
         popupBox.classList.add('scale-100');
-    }, 600);
+    }, 4500);
     function hidePopup() {
         popup.classList.remove('opacity-100', 'pointer-events-auto');
         popupBox.classList.remove('scale-100');
