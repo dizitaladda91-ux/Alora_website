5c96aa835ed5dc10df7a15ee0b4b3ee6b8bf9696
satyam07july-png <satyam07july@gmail.com>
2026-09-02 14:57:31 +0530

    inital chnages

--- backend/services/sitemap.service.js
+++ backend/services/sitemap.service.js
@@ -26,7 +26,7 @@ export async function generateSitemapXml() {
     ];
 
     const today = new Date().toISOString().split('T')[0];
-    let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
+    let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
 
     // 1. Static Pages
     staticPages.forEach(page => {
